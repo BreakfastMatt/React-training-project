6135be4958fdceb95eb72f029d997c40ed4354fe
Matthew Lewis <Mlewis@SINGULAR.CO.ZA>
2021-02-15 10:27:04 +0200

    Skeleton code and project in place for andrews task

---
+++ 4) Andrews Tasks/andrews_tasks/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render(){
+        //Render stuffs
+        return (
+            <div>
+                App component here
+            </div>
+        );
+    }
+}
+
+export default App;
